2d7fa9b0fc3144084737ef6de899b747daa5983a
Musa Martin BALOYI <mbaloyi@e4r8p7.wethinkcode.co.za>
2018-10-03 12:58:46 +0200

    update

--- Day01/ex09/ssap2.php
+++ Day01/ex09/ssap2.php
@@ -6,14 +6,14 @@ foreach ($argv as $arr)
     $newstr = array_merge($newstr, explode(" ", $arr));
 natcasesort($newstr);
 foreach ($newstr as $v)
-    if(ctype_alpha($v))
+    if(ctype_alpha($v[0]))
         echo $v . "\n";
 rsort($newstr);
 foreach ($newstr as $v)
-    if (ctype_digit($v))
+    if (ctype_digit($v[0]))
     echo $v . "\n";
 sort($newstr);
 foreach ($newstr as $v)
-    if (!ctype_alnum($v) && !ctype_alpha($v))
+    if (!ctype_alnum($v[0]) && !ctype_alpha($v[0]))
     echo $v . "\n";
 ?>
